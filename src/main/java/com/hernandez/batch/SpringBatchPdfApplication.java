6becf4e45f0f496c8ae57581c32d6270d954d91a
Jose Hernandez <jhernandez@gruposolux.com>
2019-09-25 16:29:29 -0300

    version estable para generar pdf con batch

---
+++ src/main/java/com/hernandez/batch/SpringBatchPdfApplication.java
@@ -0,0 +1,13 @@
+package com.hernandez.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPdfApplication.class, args);
+	}
+
+}
